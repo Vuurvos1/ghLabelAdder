16c9bd474ef998f73b8f886c28a0dcae4f5f1ad4
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-02-28 20:34:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ghTagAdder
- add tags based on user to your gh repo
+
+add tags based on user to your gh repo
+
+## Setup
+
+1. Create a `.env` file containing
+
+```env
+GIT_OATH=Github oath key
+REPO=Repository name
+REPO_OWNER=Owner of repository
+```
+
+2. Create a `config.json` file in the root of repository containing this structure:
+
+```json
+[
+  {
+    "role": "rollname-1",
+    "asignees": [
+      "user1",
+      "user2",
+      "user3",
+      ...
+      ]
+  },
+  {
+    "role": "rollname-2",
+    "asignees": [
+      "user1",
+      ...
+    ]
+  },
+  ...
+]
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { Octokit } = require('@octokit/rest');
+const roles = require('./config.json');
+
+const { REPO_OWNER, REPO } = process.env;
+
+// create dictionary
+const roleAssignments = {};
+roles.forEach((el) => {
+  el.asignees.forEach((member) => {
+    roleAssignments[member] = el.role;
+  });
+});
+
+const octokit = new Octokit({
+  auth: process.env.GIT_OATH,
+});
+
+// main();
+
+async function main() {
+  const issues = await octokit.issues.listForRepo({
+    owner: REPO_OWNER,
+    repo: REPO,
+  });
+
+  for (const issue of issues.data) {
+    const label = roleAssignments[issue.user.login];
+    let labelFound = false;
+
+    for (const _label of issue.labels) {
+      if (_label.name == label) {
+        labelFound = true;
+        break;
+      }
+    }
+
+    // no matching label was found -> add label
+    if (!labelFound) {
+      await octokit.issues.addLabels({
+        owner: REPO_OWNER,
+        repo: REPO,
+        issue_number: issue.number,
+        labels: [label],
+      });
+    }
+  }
+}
